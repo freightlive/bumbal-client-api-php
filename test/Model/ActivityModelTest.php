a65ffce2f11d2ccf64cc3ad903691c126c1c3950
freightlive <gerb@bumbal.eu>
2017-08-18 10:24:10 +0000

    Generated code by SwaggerHub

--- test/Model/ActivityModelTest.php
+++ test/Model/ActivityModelTest.php
@@ -400,6 +400,22 @@ class ActivityModelTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test attribute "assignment_link"
+     */
+    public function testPropertyAssignmentLink()
+    {
+
+    }
+
+    /**
+     * Test attribute "route_link"
+     */
+    public function testPropertyRouteLink()
+    {
+
+    }
+
+    /**
      * Test attribute "tags"
      */
     public function testPropertyTags()
