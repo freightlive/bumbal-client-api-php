c92a0b31d8be90fb9c6a4f30045442b7a18ee8ae
Bumbal CodeGen <gerb@freightlive.eu>
2021-07-28 13:30:55 +0200

    General Update

--- docs/Model/PackageLineRetrieveListArguments.md
+++ docs/Model/PackageLineRetrieveListArguments.md
@@ -11,6 +11,7 @@ Name | Type | Description | Notes
 **sorting_direction** | **string** | Sorting Direction | [optional] 
 **search_text** | **string** |  | [optional] 
 **as_list** | **bool** |  | [optional] 
+**count_only** | **bool** |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
--- src/Model/PackageLineRetrieveListArguments.php
+++ src/Model/PackageLineRetrieveListArguments.php
@@ -61,7 +61,8 @@ class PackageLineRetrieveListArguments implements ArrayAccess
         'sorting_column' => 'string',
         'sorting_direction' => 'string',
         'search_text' => 'string',
-        'as_list' => 'bool'
+        'as_list' => 'bool',
+        'count_only' => 'bool'
     ];
 
     /**
@@ -76,7 +77,8 @@ class PackageLineRetrieveListArguments implements ArrayAccess
         'sorting_column' => null,
         'sorting_direction' => null,
         'search_text' => null,
-        'as_list' => null
+        'as_list' => null,
+        'count_only' => null
     ];
 
     public static function swaggerTypes()
@@ -101,7 +103,8 @@ class PackageLineRetrieveListArguments implements ArrayAccess
         'sorting_column' => 'sorting_column',
         'sorting_direction' => 'sorting_direction',
         'search_text' => 'search_text',
-        'as_list' => 'as_list'
+        'as_list' => 'as_list',
+        'count_only' => 'count_only'
     ];
 
 
@@ -117,7 +120,8 @@ class PackageLineRetrieveListArguments implements ArrayAccess
         'sorting_column' => 'setSortingColumn',
         'sorting_direction' => 'setSortingDirection',
         'search_text' => 'setSearchText',
-        'as_list' => 'setAsList'
+        'as_list' => 'setAsList',
+        'count_only' => 'setCountOnly'
     ];
 
 
@@ -133,7 +137,8 @@ class PackageLineRetrieveListArguments implements ArrayAccess
         'sorting_column' => 'getSortingColumn',
         'sorting_direction' => 'getSortingDirection',
         'search_text' => 'getSearchText',
-        'as_list' => 'getAsList'
+        'as_list' => 'getAsList',
+        'count_only' => 'getCountOnly'
     ];
 
     public static function attributeMap()
@@ -207,6 +212,7 @@ class PackageLineRetrieveListArguments implements ArrayAccess
         $this->container['sorting_direction'] = isset($data['sorting_direction']) ? $data['sorting_direction'] : null;
         $this->container['search_text'] = isset($data['search_text']) ? $data['search_text'] : null;
         $this->container['as_list'] = isset($data['as_list']) ? $data['as_list'] : null;
+        $this->container['count_only'] = isset($data['count_only']) ? $data['count_only'] : null;
     }
 
     /**
@@ -440,6 +446,27 @@ class PackageLineRetrieveListArguments implements ArrayAccess
     public function setAsList($as_list)
     {
         $this->container['as_list'] = $as_list;
+
+        return $this;
+    }
+
+    /**
+     * Gets count_only
+     * @return bool
+     */
+    public function getCountOnly()
+    {
+        return $this->container['count_only'];
+    }
+
+    /**
+     * Sets count_only
+     * @param bool $count_only 
+     * @return $this
+     */
+    public function setCountOnly($count_only)
+    {
+        $this->container['count_only'] = $count_only;
 
         return $this;
     }
